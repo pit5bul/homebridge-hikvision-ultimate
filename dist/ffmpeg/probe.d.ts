5492c72f0a9b5c13123293fe71d0e6b65b48ffc0
pit5bul <petrut@popa.com.au>
2026-04-04 06:09:36 +0000

    chore: add .gitignore, remove node_modules and dist from tracking

--- dist/ffmpeg/probe.d.ts
+++
@@ -1,6 +0,0 @@
-import { Logger } from 'homebridge';
-import { DetectedStreamInfo } from '../configTypes';
-/**
- * Run ffprobe on a stream URL and return detected information
- */
-export declare function probeStream(ffprobePath: string, streamUrl: string, timeout: number, log: Logger): Promise<DetectedStreamInfo | null>;
--- dist/index.d.ts
+++
@@ -1,6 +0,0 @@
-import { API } from 'homebridge';
-/**
- * Register the platform with Homebridge
- */
-declare const _default: (api: API) => void;
-export default _default;
